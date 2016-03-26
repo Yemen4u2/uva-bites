033fe54c7ec13037490c4a98c9d2b13a0f2372cb
Salah Assana <Salah Assana>
2016-03-26 17:57:17 -0400

    able to connect to back end now

--- search.php
+++ search.php
@@ -4,12 +4,28 @@
       $password = "p@ssw0rd";
       $database = "cs4750jgd3hb";
 
+      $test = $_GET["restaurantName"];
+
       // Create connection
-      $conn = new mysqli($servername, $username, $password);
+      $conn = new mysqli($servername, $username, $password, $database);
 
       // Check connection
-      if ($conn->connect_error) {
+      if ($conn->connect_error) 
+      {
           die("Connection failed: " . $conn->connect_error);
       } 
+
       echo "Connected successfully";
+
+      $result = mysqli_query($conn, "SELECT restaurant_name FROM Restaurant WHERE restaurantID = '$test'");
+      
+      if(!$result) 
+      {
+            die("Database query failed: " . mysqli_error($conn));
+      }
+      
+      while ($row = mysqli_fetch_array($result)) 
+      {
+            echo "We found it!!!";
+      }
 ?>
