3e6506e0e72e83553afe1ab2b898e8b49b0279a4
Maggie Negm <maggiehn94@yahoo.com>
2016-04-14 22:08:32 -0400

    created signUp and adminLogin pages w/ their respective checks

---
+++ memberHome.php
@@ -0,0 +1,15 @@
+<?php
+	session_start();
+
+	echo "hello";
+
+	/*
+	if(isset($_SESSION["userName"])) {
+		$userName = $_SESSION["userName"];
+	} else {
+		header("Location: adminLogin.html");
+	}
+
+	echo "Welcome" . $userName;
+	*/
+?>
--- signUp.php
+++ signUp.php
@@ -1,7 +1,10 @@
  <?php
  	//authentication - checking owner DB to see if users have accounts (checking email and password)
 
- 	session_start();
+ 	if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    }
 
 	$userName = rtrim(strip_tags($_POST["userName"]));
 	$passW = rtrim(strip_tags($_POST["password"]));
@@ -18,19 +21,18 @@
 		die ("Could not connect to db: " . mysqli_connect_errno());
 	endif;
 
+	//check if user already has an account
 	$query = "SELECT * FROM Admins WHERE User_Name='$userName' AND Password='$passW'";
 	$result = mysqli_query($db, $query) or die ("Invalid Query: " . mysqli_error($db));
 	$rows = mysqli_num_rows($result);
 
-	if ($rows < 1):
-		echo "You are not an authorized user. Please try again.";
-		include "adminLogin.html";
+	if($rows < 1) { //user does not exist in table
+		$query = "INSERT INTO Admins (User_Name, Password) VALUES ('$userName', '$passW')";
+		$result = mysqli_query($db, $query) or die ("Invalid Query: " . mysqli_error($db));
+		header("Location: http://localhost/uva-bites/memberHome.php");
+	} else {
+		echo "You are already registered. Please login in.";
+		include "signUp.html";
 		session_destroy();
-	else:
-		//otherwise, user is an authorized user
-		$row = $result->fetch_array();
-		$name = stripslashes($row["User_Name"]);
-		$_SESSION["userName"] = $name; //create session variable and store user name
-		header("Location: memberHome.php");
-	endif;
+	}
 ?>
