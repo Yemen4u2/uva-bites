a31a15fa9b0d3e6003983b80cdc0c1e04bc78f08
Salah Assana <Salah Assana>
2016-04-16 19:28:09 -0400

    updated sign up and sign in pages

--- memberHome.php
+++ memberHome.php
@@ -6,6 +6,52 @@
 	} else {
 		header("Location: adminLogin.html");
 	}
+
+	//Setting up Connection to the database.
+
+	$servername = "stardock.cs.virginia.edu";
+	$username = "cs4750jgd3hb";
+	$password = "p@ssw0rd";
+	$database = "cs4750jgd3hb";
+
+	 // Create connection
+	$conn = new mysqli($servername, $username, $password, $database);
+
+	$AdminResult = mysqli_query($conn, "SELECT * FROM Admins WHERE User_Name = '$userName'");
+	$AdminsRow = mysqli_fetch_array($AdminResult);
+	$restaurantID = $AdminsRow['restaurantID'];
+
+	//Creat table by natural joining restaurant, 
+
+    $ResturantInformationResult = mysqli_query($conn, "SELECT * 
+    						FROM Restaurant NATURAL JOIN Dining NATURAL JOIN Parking NATURAL JOIN Rating
+							WHERE restaurantID = '$restaurantID'");
+	$ResturantInformationRow = mysqli_fetch_assoc($ResturantInformationResult);
+	$restaurant_name = $ResturantInformationRow['restaurant_name'];
+    $cusine = $ResturantInformationRow['cusine'];
+    $wait_time = $ResturantInformationRow['wait_time'];
+    $rating = $RestaurantInformationRow['numberOfStars'];
+
+    $RestaurantOutput = "<b> Resturant Name: </b> $restaurant_name <br />
+    					 <b> Cusine: </b> $cusine <br />
+    					 <b> Average Wait Time: </b> $wait_time Minutes <br />
+    					 <b> Rating: </b> $rating <br />";
+
+    //Creat table by natural joining Contact and Location table to find the restaurant's email, number and address.
+
+    $ResturantContactResult = mysqli_query($conn, "SELECT * FROM Contact NATURAL JOIN Location
+					WHERE restaurantID = '$restaurantID'");
+	$ResturantContactRow = mysqli_fetch_array($ResturantContactResult);
+    $resturantPhoneNumber = $ResturantContactRow['phone'];
+    $restaurantEmail = $ResturantContactRow['email'];
+    $restaurantStreet = $ResturantContactRow['street'];
+    $restaurantZipCode = $ResturantContactRow['zip'];
+
+    $ContactOutput = "<b> Phone: </b> $resturantPhoneNumber <br />
+    				  <b> Email: </b> $restaurantEmail <br />
+    				  <b> Street Address: </b> $restaurantStreet <br />
+    				  <b> Zip Code: </b> $restaurantZipCode";
+
 ?>
 <!DOCTYPE html>
 <!--
@@ -33,26 +79,19 @@
 				</nav>		
 			</header>
 		</div>
-		<center>
-			<section id="main" class="container 75%">
-				<header>
-					<h3>Welcome <?php echo $userName; ?>!</h3>
-				</header>
-				<div class="box">
-					<form action="choiceForms.php" method="POST">
-						<div class="row uniform 50%">
-							<div class="12u">
-								<h4>Would you like to:</h4>
-								<input type="radio" name="choice" value="add">create a new restuarant<br>
-								<input type="radio" name="choice" value="create">add a new component to your restuarant<br>
-						  		<input type="radio" name="choice" value="modify">update a component of your existing restaurant<br>
-						  		<br>
-						  		<input type="submit" value="Enter">
-							</div>
+
+		<section id="main" class="container 75%">
+			<header>
+				<h3>Welcome <?php echo $userName; ?>!</h3>
+			</header>
+					<div class="row uniform 50%">
+						<div class="12u">
+						<?php
+						  	echo "<div class=\"box\">$RestaurantOutput</div>";
+						  	echo "<div class=\"box\">$ContactOutput</div>";
+						?>
 						</div>
-					</form>
-				</div>
-			</section>
-		</center>									
+					</div>
+		</section>								
 	</body>
 </html>
--- signUp.php
+++ signUp.php
@@ -1,15 +1,94 @@
  <?php
- 	//authentication - checking owner DB to see if users have accounts (checking email and password)
 
- 	if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-    }
-
+	
+ 	//Setting variables equal to user input
 	$userName = rtrim(strip_tags($_POST["userName"]));
 	$passW = rtrim(strip_tags($_POST["password"]));
+	$restaurantName = rtrim(strip_tags($_POST["restaurant"]));
+	$streetAddress = rtrim(strip_tags($_POST["streetName"]));
+	$zipCode =  rtrim(strip_tags($_POST["zipCode"]));
+	$cusine = rtrim(strip_tags($_POST["cusine"]));
+	$waitTime = rtrim(strip_tags($_POST["waitTime"]));
+	$restaurantRating = rtrim(strip_tags($_POST["rating"]));
+	$restaurantCost = rtrim(strip_tags($_POST["cost"]));
+	$restaurantPhoneNumber = rtrim(strip_tags($_POST["contactNumber"]));
+	$restaurantEmail = rtrim(strip_tags($_POST["contactEmail"]));
+	$costOfParking = rtrim(strip_tags($_POST["parkingCost"]));
 
-	//connect to MySQL DB
+	//Now we check to see which boxes the user checked. 
+	//The variables will be set to true (1) or false (0) based on that.
+
+	if (isset($_POST['sitIn']) && $_POST['sitIn'] == '1')
+	{
+		$sitIn = 1;
+	}
+	else
+	{
+		$sitIn = 0;
+	}
+
+	if (isset($_POST['delivery']) && $_POST['delivery'] == '1')
+	{
+		$delivery = 1;
+	}
+	else
+	{
+		$delivery = 0;
+	}
+
+	if (isset($_POST['carryout']) && $_POST['carryout'] == '1')
+	{
+		$carryout = 1;
+	}
+	else
+	{
+		$carryout = 0;
+	}
+
+	if (isset($_POST['reservation']) && $_POST['reservation'] == '1')
+	{
+		$reservation = 1;
+	}
+	else
+	{
+		$reservation = 0;
+	}
+
+	//Same as above except this is for the parking options
+
+	if (isset($_POST['parkingLot']) && $_POST['parkingLot'] == '1')
+	{
+		$parkingLot = 1;
+	}
+	else
+	{
+		$parkingLot = 0;
+	}
+
+	if (isset($_POST['streetParking']) && $_POST['streetParking'] == '1')
+	{
+		$streetParking = 1;
+	}
+	else
+	{
+		$streetParking = 0;
+	}
+
+	if (isset($_POST['garageParking']) && $_POST['garageParking'] == '1')
+	{
+		$garageParking = 1;
+	}
+	else
+	{
+		$garageParking = 0;
+	}
+
+
+	//ALL VARIABLES HAVE BEEN SET
+
+
+	//Connect to MySQL DB
+
 	$servername = "stardock.cs.virginia.edu";
     $username = "cs4750jgd3hb";
     $password = "p@ssw0rd";
@@ -21,18 +100,69 @@
 		die ("Could not connect to db: " . mysqli_connect_errno());
 	endif;
 
-	//check if user already has an account
-	$query = "SELECT * FROM Admins WHERE User_Name='$userName' AND Password='$passW'";
+	//check if user already has an account looking through the database for a matching user
+
+	$query = "SELECT * FROM Admins WHERE User_Name='$userName'";
 	$result = mysqli_query($db, $query) or die ("Invalid Query: " . mysqli_error($db));
 	$rows = mysqli_num_rows($result);
 
-	if($rows < 1) { //user does not exist in table
-		$query = "INSERT INTO Admins (User_Name, Password) VALUES ('$userName', '$passW')";
-		$result = mysqli_query($db, $query) or die ("Invalid Query: " . mysqli_error($db));
-		header("Location: http://localhost/uva-bites/memberHome.php");
-	} else {
+	//if the user does not alread exist in table there will be no rows found
+
+	if($rows < 1)
+	{ 
+
+		//Calculate the total number of restaurants in the database to assign new restaurant ID
+
+		$totalResaurant = "SELECT * FROM Restaurant";
+		$result = mysqli_query($db, $totalResaurant);
+		$restaurantID = mysqli_num_rows($result) + 1;
+
+		//Add restaurant information to database
+		
+		$addRestaurant = "INSERT INTO Restaurant (restaurantID, restaurant_name, cusine, wait_time) 
+						VALUES ('$restaurantID', '$restaurantName', '$cusine', '$waitTime')";
+		mysqli_query($db, $addRestaurant);
+
+		//Add user information to database
+
+		$addUser = "INSERT INTO Admins (User_Name, Password, restaurantID) 
+					VALUES ('$userName', '$passW', '$restaurantID')";
+		mysqli_query($db, $addUser);
+
+		//Add contact information to database
+
+		$addContactInformation = "INSERT INTO Contact (restaurantID, phone, email) 
+								VALUES ('$restaurantID', '$restaurantPhoneNumber', '$restaurantEmail')";
+		mysqli_query($db, $addContactInformation);
+
+		//Add dining options to database
+
+		$addDiningOptions = "INSERT INTO Dining (restaurantID, sitin, delivery, carryout, reservation)
+							VALUES ('$restaurantID', '$sitIn', '$delivery', '$carryout', '$carryout', '$reservation')";
+		mysqli_query($db, $addDiningOptions);
+
+		//Add location information to database
+
+		$addRestaurantLocation = "INSERT INTO Location (restaurantID, street, zip)
+								VALUES ('$restaurantID', '$streetAddress', '$zipCode')";
+		mysqli_query($db, $addRestaurantLocation);
+
+		//Add parking option to database
+
+		$addParkingOptions = "INSERT INTO Parking (restaurantID, ParkingLot, StreetParking, Garage, Cost)
+						VALUES ('$restaurantID', '$parkingLot', '$streetParking', '$garageParking', '$costOfParking')";
+		mysqli_query($db, $addParkingOptions);
+
+		//Add restaurant rating to database
+
+		$addRestaurantRating = "INSERT INTO Rating (restaurantID, numberOfStars) 
+							VALUES ('$restaurantID', '$restaurantRating')";
+		mysqli_query($db, $addRestaurantRating);
+		
+		header("Location: http://localhost/uva-bites/adminLogin.html");
+	}
+	else
+	{
 		echo "You are already registered. Please login in.";
-		include "signUp.html";
-		session_destroy();
 	}
 ?>
